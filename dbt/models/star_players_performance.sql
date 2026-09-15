0d1e2167d17287f6b14777cb1b9bb27e22d6bed7
Daniel Bubalo <danielbubalo45@gmail.com>
2026-09-15 21:55:52 +0200

    added star_player_performance model with first_award_season and schema tests

---
+++ dbt/models/star_players_performance.sql
@@ -0,0 +1,32 @@
+WITH
+    star_players AS (
+        SELECT
+            *
+        FROM
+            {{ref ('player_clutch_performance')}}
+        WHERE
+            group_tier IN ('Star', 'Olympic Gold Medalist')
+    ),
+    award_seasons AS (
+        SELECT
+            player_id,
+            MIN(season) AS first_award_season
+        FROM
+            {{source ('raw', 'player_awards')}}
+        WHERE
+            LENGTH(season) = 7
+        GROUP BY
+            player_id
+    ),
+    with_first_award AS (
+        SELECT
+            star.*,
+            awards.first_award_season
+        FROM
+            star_players AS star
+            JOIN award_seasons AS awards ON star.player_id = awards.player_id
+    )
+SELECT
+    *
+FROM
+    with_first_award
